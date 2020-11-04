b627492bdffa5ffbf1c3228f37855302023ee516
Denis Serdyuk <diserdyuk@gmail.com>
2020-11-04 15:18:11 +0200

    Write data to data base, var 1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from peewee import *
 db = PostgresqlDatabase(database='db_test', user='postgres', password='1', host='localhost')     # connector orm with db 
 
 
-class Finance(Model):
+class Stock(Model):
     symbol = CharField()    # class fields
     price = CharField()
     url = TextField()
@@ -18,16 +18,21 @@ class Finance(Model):
 def main():
 
     db.connect()    # connect to database
-    db.create_tables([Finance])    # create shreadsheet in database
+    db.create_tables([Stock])    # create shreadsheet in database
 
     with open('finance_yahoo.csv') as f:
         order = ['symbol', 'price', 'url']
         read = csv.DictReader(f, fieldnames=order)    # get dictionares 
 
-        data = list(read)    # dictreader to list type
+        stocks = list(read)    # dictreader to list type
 
-        for i in data:
-            print(i)
+        # var_1 to write in db   
+        for i in stocks: 
+            stock = Stock(symbol=i['symbol'], price=i['price'], url=i['url'])
+            stock.save()
+
+        # var_2, context manager
+        # with db.atomic()
 
 
 
