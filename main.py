7142ba4435ae9176c76f16e211411e46770bb51a
Denis Serdyuk <diserdyuk@gmail.com>
2020-11-04 12:42:07 +0200

    Create and connect database

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import csv
+from peewee import *
+
+
+db = PostgresqlDatabase(database='db_test', user='postgres', password='1', host='localhost')     # connector orm with db 
+
+
+class Finance(Model):
+    symbol = CharField()    # class fields
+    price = CharField()
+    url = TextField()
+
+    class Meta:    # connect with db
+        database = db
+
 
 
 def main():
+
+    db.connect()    # connect to database
+    db.create_tables([Finance])    # create shreadsheet in database
+
     with open('finance_yahoo.csv') as f:
         order = ['symbol', 'price', 'url']
-        read = csv.DictReader(f, fieldnames=order)
+        read = csv.DictReader(f, fieldnames=order)    # get dictionares 
 
-        for i in read:
+        data = list(read)    # dictreader to list type
+
+        for i in data:
             print(i)
 
 
